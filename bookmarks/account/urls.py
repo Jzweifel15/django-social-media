ab2d8548be9ff6fa89eddc1e8c4153609e2765db
Jzweifel15 <jzweifel15@yahoo.com>
2021-04-19 19:16:10 -0400

    Password Reset URLs
    Added the URLs that allow a user to reset their password using Django's built-in authentication framework views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,9 +7,14 @@ urlpatterns = [
   # Log in and Log out URLs
   path("login/", auth_views.LoginView.as_view(), name="login"),
   path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+  # User Dashboard URL
+  path("", views.dashboard, name="dashboard"),
   # Change Password URLs
   path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
   path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
-  # User Dashboard URL
-  path("", views.dashboard, name="dashboard")
+  # Reset Password URLs
+  path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+  path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+  path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+  path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
