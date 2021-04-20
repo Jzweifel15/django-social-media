dd9e2b4eb087fc703a3ccf049b37823d740f4ebd
Jzweifel15 <jzweifel15@yahoo.com>
2021-04-19 20:54:15 -0400

    Update views.py
    Added the view that will allow authenticated users to edit their profile

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
 def user_login(request):
@@ -36,6 +36,7 @@ def user_login(request):
   return render(request, "account/login.html", {"form": form})
 
 
+# Users must be authenticated before accessing the dashboard
 @login_required
 def dashboard(request):
   return render(request, "account/dashboard.html", {"section": "dashboard"})
@@ -63,3 +64,24 @@ def register(request):
     user_form = UserRegistrationForm()
 
   return render(request, "account/register.html", {"user_form": user_form})
+
+
+# Users must be authenticated to edit their profile
+@login_required
+def edit(request):
+  if request.method == "POST":
+    user_form = UserEditForm(instance=request.user, data=request.POST)
+    profile_form = ProfileEditForm(instance=request.user.profile,
+                                   data=request.POST,
+                                   files=request.FILES)
+
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+
+  else:
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+
+  return render(request, "account/edit.html", {"user_form": user_form,
+                                               "profile_form": profile_form})                                   
